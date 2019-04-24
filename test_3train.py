5bd7747bfa4b7de7cb850f6991ed9a3375fa42b4
gusbeane <abeane@sas.upenn.edu>
2019-04-24 10:27:13 -0400

    Small fixes

--- test_3train.py
+++ test_3train.py
@@ -2,7 +2,7 @@ import numpy as np
 from mnist import MNIST
 
 import matplotlib.pyplot as plt
-from hopfield import hopfield
+from hopfield import hopfield, choose_training, choose_test, massage_images
 
 if __name__ == '__main__':
     mndata = MNIST('samples')
@@ -23,14 +23,14 @@ if __name__ == '__main__':
         test_img = np.reshape(test_images[i], (28,28))
 
         plt.imshow(test_img)
-        plt.savefig('images_2train/input_test_'+str(i)+'.png')
+        plt.savefig('images_3train/input_test_'+str(i)+'.png')
         plt.close()
 
     for i in range(len(train_keys)):
         train_img = np.reshape(train_images[i], (28,28))
 
         plt.imshow(train_img)
-        plt.savefig('images_2train/train_'+str(i)+'.png')
+        plt.savefig('images_3train/train_'+str(i)+'.png')
         plt.close()
 
     h = hopfield(train_images[[1,3,8]], test_images, theta=0, nprocess=5000, storkey=False)
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         img = np.reshape(retrain_img[i], (28,28))
 
         plt.imshow(img)
-        plt.savefig('images_2train/retrain_'+str(i)+'.png')
+        plt.savefig('images_3train/retrain_'+str(i)+'.png')
         plt.close()
 
     for i in range(len(test_keys)):
@@ -49,9 +49,9 @@ if __name__ == '__main__':
         proc_img = np.reshape(h.processed_data[i], (28,28))
 
         plt.imshow(test_img)
-        plt.savefig('images_2train/test_'+str(i)+'.png')
+        plt.savefig('images_3train/test_'+str(i)+'.png')
         plt.close()
 
         plt.imshow(proc_img)
-        plt.savefig('images_2train/proc_'+str(i)+'.png')
+        plt.savefig('images_3train/proc_'+str(i)+'.png')
         plt.close()
